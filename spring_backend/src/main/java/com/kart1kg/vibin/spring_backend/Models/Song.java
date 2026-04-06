309979bb4ad52f608caf910263868bcd34040018
Kartik <154257621+kart1k-g@users.noreply.github.com>
2026-04-06 19:15:49 +0530

    create song model -uses lombok to generate getters and setters

--- spring_backend/src/main/java/com/kart1kg/vibin/spring_backend/Models/Song.java
+++ spring_backend/src/main/java/com/kart1kg/vibin/spring_backend/Models/Song.java
@@ -1,5 +1,35 @@
 package com.kart1kg.vibin.spring_backend.Models;
 
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import lombok.Data;
+
+@Data
+@Entity
 public class Song{
+    @Id
+    @GeneratedValue()
+    @UuidGenerator
+    @Column(name="song_id")
+    private UUID id;
     
+    @Column(name="song_url")
+    private String songUrl;
+
+    @Column(name="thumbnail_url")
+    private String thumbnailUrl;
+
+    @Column(name="artist_name")
+    private String artist;
+
+    @Column(name="song_name")
+    private String name;
+
+    private String hexcode;
 }
